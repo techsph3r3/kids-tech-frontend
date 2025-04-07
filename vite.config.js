b0dc9a911931fa67dbb7ebc60ca518c9fc1eb85d
techsph3r3 <tech@sph3r3.com>
2025-04-07 17:13:49 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,9 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  root: '.',           // ensure root is correct
   build: {
-    outDir: 'dist',    // where to output the final build
-    emptyOutDir: true, // clean before build
-  },
-  publicDir: 'public', // explicitly use the public folder
+    outDir: 'dist',
+  }
 });
