60d583fa974497920aa799c53c5214a2f9d40f6e
techsph3r3 <tech@sph3r3.com>
2025-04-07 18:51:12 -0500

    Create server.js

---
+++ server.js
@@ -0,0 +1,21 @@
+import express from 'express';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const app = express();
+const port = process.env.PORT || 8080;
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+// Serve files from /dist
+app.use(express.static(path.join(__dirname, 'dist')));
+
+// Redirect all other routes to index.html (for React router)
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'dist', 'index.html'));
+});
+
+app.listen(port, () => {
+  console.log(`✅ Serving React app on port ${port}`);
+});
