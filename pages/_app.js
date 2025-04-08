a9a66e5c4f319d2bf1ef0f23229a9c63b108a8d2
techsph3r3 <tech@sph3r3.com>
2025-04-08 12:38:02 -0500

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,28 @@
-import { PublicClientApplication } from "@azure/msal-browser";
+import { useEffect } from "react";
+import { PublicClientApplication, EventType } from "@azure/msal-browser";
 import { MsalProvider } from "@azure/msal-react";
 import { msalConfig } from "../lib/authConfig";
 
 const msalInstance = new PublicClientApplication(msalConfig);
 
+// Optional: handle redirect to close the popup
+if (typeof window !== "undefined") {
+  msalInstance.addEventCallback((event) => {
+    if (event.eventType === EventType.LOGIN_SUCCESS && event.payload.account) {
+      msalInstance.setActiveAccount(event.payload.account);
+    }
+  });
+}
+
 function MyApp({ Component, pageProps }) {
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      msalInstance
+        .handleRedirectPromise()
+        .catch((err) => console.error("MSAL redirect error:", err));
+    }
+  }, []);
+
   return (
     <MsalProvider instance={msalInstance}>
       <Component {...pageProps} />
