91c32b4ccb811cdfe6a4222d79a95695c2cebc5e
techsph3r3 <tech@sph3r3.com>
2025-04-07 21:29:00 -0500

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import { PublicClientApplication } from '@azure/msal-browser';
+import { MsalProvider } from '@azure/msal-react';
+import { msalConfig } from './authConfig';
 
-console.log("🟢 main.jsx is running!");
+const msalInstance = new PublicClientApplication(msalConfig);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <MsalProvider instance={msalInstance}>
+      <App />
+    </MsalProvider>
   </React.StrictMode>
 );
