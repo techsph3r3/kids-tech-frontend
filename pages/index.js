514a86fd11bde66b087a7fbade896efc921db674
techsph3r3 <tech@sph3r3.com>
2025-04-08 12:23:37 -0500

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,10 @@
+import LoginButton from "../components/LoginButton";
+
+export default function Home() {
+  return (
+    <main style={{ padding: "2rem", fontFamily: "sans-serif" }}>
+      <h1>Kids Tech Frontend</h1>
+      <LoginButton />
+    </main>
+  );
+}
