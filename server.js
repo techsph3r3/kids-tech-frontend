4a6905124717aa8632b9cabf2a03628f21255a84
techsph3r3 <tech@sph3r3.com>
2025-04-07 19:03:58 -0500

    Update server.js

--- server.js
+++ server.js
@@ -8,14 +8,12 @@ const port = process.env.PORT || 8080;
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-// Serve files from /dist
 app.use(express.static(path.join(__dirname, 'dist')));
 
-// Redirect all other routes to index.html (for React router)
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'dist', 'index.html'));
 });
 
 app.listen(port, () => {
-  console.log(`✅ Serving React app on port ${port}`);
+  console.log(`✅ Frontend is live on port ${port}`);
 });
